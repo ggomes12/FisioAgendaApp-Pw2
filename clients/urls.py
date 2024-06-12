04c0050089be83d0d4d0a5df64ef149dc77132a6
MarcosAndreLS <marcosofc04@gmail.com>
2024-06-12 14:06:37 -0300

    Redefinição de senha - atualizações

--- clients/urls.py
+++ clients/urls.py
@@ -1,31 +1,31 @@
-# clients/urls.py
 from django.urls import path, reverse_lazy
 from django.contrib.auth import views as auth_views
-from . import views
+from clients import views
+
+
 
 urlpatterns = [
      path('login/', views.login_clients, name='login_clients'),
-     path('logout/', views.logout_clients, name='logout_clients'),
      path('login_profissional/', views.login_profissional,
           name='login_prof'),
+     path('logout_clients/', views.logout_clients, name='logout_clients'),
+     path('logout_prof/', views.logout_prof, name='logout_prof'),
      path('profissionais_lista/', views.profissionais_lista,
           name='profissionais_lista'),
      path('profile_prof/', views.profile_prof, name='profile_prof'),
-     path('profile_client/', views.profile_client, name='profile_client'),
+     path('profile_clients/', views.profile_client, name='profile_clients'),
      path('registration_profissional/', views.registration_profissional,
           name='registration_prof'),
      path('registration_client/', views.registration_client,
           name='registration_client'),
-
-    
+     #path('esqueceu_senha/', views.esqueceu_senha, name='esqueceu_senha'),
+     path('404/', views.pagina_nao_encontrada, name='pagina_nao_encontrada'),
+     path('portfolio_details/', views.portfolio_details, name='portfolio_details'),
+     path('news_blog/', views.news_blog, name='news_blog'),
      path("password_change", auth_views.PasswordChangeView.as_view(template_name='password_change_form.html'), name="password_change"),
      path("password_change_done/", auth_views.PasswordChangeDoneView.as_view(template_name='password_change_done.html'), name="password_change_done"),
      path('esqueceu_senha/', auth_views.PasswordResetView.as_view(template_name='esqueceu_senha.html'), name='esqueceu_senha'),
      path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'), name='password_reset_done'),
      path('password_reset_confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'), name='password_reset_confirm'),
      path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'), name='password_reset_complete'),
-
-     path('404/', views.pagina_nao_encontrada, name='pagina_nao_encontrada'),
-     path('portfolio_details/', views.portfolio_details, name='portfolio_details'),
-     path('news_blog/', views.news_blog, name='news_blog'),
 ]
--- clients/views.py
+++ clients/views.py
@@ -1,4 +1,6 @@
-from django.contrib.auth import authenticate, login, logout
+from django.core.exceptions import ValidationError
+from django.contrib.auth import login, logout
+from django.contrib import messages
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import AuthenticationForm
