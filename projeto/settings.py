353ff96375a121b205616f05d438869528108c0e
MarcosAndreLS <marcosofc04@gmail.com>
2024-06-17 17:55:29 -0300

    login por email

--- projeto/settings.py
+++ projeto/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-isegur_2ds7&x5_ai9fbpb@7088n=8p&s%z@bc!)adn_h5l=d9
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = True #mudança do debug
+DEBUG = False #mudança do debug
 
 ALLOWED_HOSTS = ['*'] #colocado o ip do site django
 
